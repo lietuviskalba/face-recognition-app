ca0b97a2673e7e04c99458ec2adaa03e6cd7f0f9
Mantas Lingaitis <mantasling@gmail.com>
2023-01-14 10:38:40 +0200

    > Image link form CSS update

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,12 +1,13 @@
 import React from "react";
-import "./ImageLinkForm.css"
+import "./ImageLinkForm.css";
+
 const ImageLinkForm = () => {
     return (
         <div>
-            <p className="f3"> {"Face detection in pictures"}</p>
+            <p className="ma0 center br3 formText"> {"Face detection in pictures"}</p>
             <div className="center">
                 <div className="pa4 br3 shadow-4 form">
-                    <input className="f44 pa2 w-70 center br3" type="text" />
+                    <input className="f44 pa2 w-70 center br3" type="text" placeholder="Enter image URL here..." />
                     <button className="mt4 w-30 grow br3 f4 link ph3 pv2 dib white bg-dark-green"> Find Face</button>
                 </div>
             </div>
