31d81a4ff2aedd1b48a1b6f6809a19446db6b90e
Mantas Lingaitis <mantasling@gmail.com>
2023-06-25 14:26:31 +0300

    >>> Updates >react tilt >react particles

--- src/App.js
+++ src/App.js
@@ -1,113 +1,42 @@
 import './App.css';
+import ParticlesBg from 'particles-bg'
+import React  from 'react';
+
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
-import { useCallback } from "react";
-import Particles from "react-tsparticles";
-import { loadFull } from "tsparticles";
+import { Component, useCallback } from "react";
 
-function App() {
+class App extends Component {
 
-  const particlesInit = useCallback(async engine => {
-    console.log(engine);
-    // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    // starting from v2 you can add only the features you need reducing the bundle size
-    await loadFull(engine);
-  }, []);
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
 
-  const particlesLoaded = useCallback(async container => {
-    await console.log(container);
-  }, []);
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
 
-  return (
+  onSubmit = () =>{
+    console.log("click");
+  }
 
-    <div className="App">
-      <Particles className='particles'
-        id="tsparticles"
-        init={particlesInit}
-        loaded={particlesLoaded}
-        options={{
-          background: {
-            color: {
-              value: "#0d47a1",
-            },
-          },
-          fpsLimit: 120,
-          interactivity: {
-            events: {
-              onClick: {
-                enable: true,
-                mode: "push",
-              },
-              onHover: {
-                enable: true,
-                mode: "repulse",
-              },
-              resize: true,
-            },
-            modes: {
-              push: {
-                quantity: 4,
-              },
-              repulse: {
-                distance: 200,
-                duration: 0.4,
-              },
-            },
-          },
-          particles: {
-            color: {
-              value: "#ffffff",
-            },
-            links: {
-              color: "#ffffff",
-              distance: 150,
-              enable: true,
-              opacity: 0.5,
-              width: 1,
-            },
-            collisions: {
-              enable: true,
-            },
-            move: {
-              directions: "none",
-              enable: true,
-              outModes: {
-                default: "bounce",
-              },
-              random: false,
-              speed: 6,
-              straight: false,
-            },
-            number: {
-              density: {
-                enable: true,
-                area: 800,
-              },
-              value: 80,
-            },
-            opacity: {
-              value: 0.5,
-            },
-            shape: {
-              type: "circle",
-            },
-            size: {
-              value: { min: 1, max: 5 },
-            },
-          },
-          detectRetina: true,
-        }}
-      />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      { /*<FaceRecognition/>} */}
-    </div>
-  );
+  render() {
+
+    return (
+      <div className="App">
+        <ParticlesBg type="circle" bg={true} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} onSubmit={this.onSubmit} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./ImageLinkForm.css";
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onSubmit}) => {
     return (
         <div>
             <p className="ma0 center br3 formText"> {"Face detection in pictures"}</p>
             <div className="center">
                 <div className="pa4 br3 shadow-4 form">
-                    <input className="f44 pa2 w-70 center br3" type="text" placeholder="Enter image URL here..." />
-                    <button className="mt4 w-30 grow br3 f4 link ph3 pv2 dib white bg-dark-green"> Find Face</button>
+                    <input className="f44 pa2 w-70 center br3" type="text" placeholder="Enter image URL here..." onChange={onInputChange}/>
+                    <button className="mt4 w-30 grow br3 f4 link ph3 pv2 dib white bg-dark-green" onClick={onSubmit}> Find Face</button>
                 </div>
             </div>
         </div>
